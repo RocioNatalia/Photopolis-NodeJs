99fc9463b5ee2c9d2e000468a3dbc66470caacff
Rocio Natalia <rocionatalia5940@gmail.com>
2019-06-24 23:43:34 -0300

    ullike

--- controllers/likesController.js
+++ controllers/likesController.js
@@ -12,20 +12,22 @@ const likesSet = async (req, res) => {
 
 //mostrar los likes del usuario por pantalla
 const likesGet = async (req, res) => {
-    let id = req.params.id;a
-    let usuario
-    let usuario = await Likes.findAll({ where: { user_id: id } })
+    let id = req.params.id;
+    let usuario = await User.findOne({ where: { id: id } })
 
-    console.log(usuario)
+    if (usuario !== null) {
 
-    if (usuario.length != 0) {
-        res.json({ response: usuario })
-    } else {
-        res.json({ response: ' No ha dado ningun like' })
+        let usuarioLikes = await Likes.findAll({ where: { user_id: id } })
+        if (usuarioLikes.length != 0) {
+            res.json({ response: usuarioLikes })
+        } else {
+            res.json({ response: ' No ha dado ningun like' })
+        }
+
+    }else {
+        res.json({ response: ' Ese usuario no existe >:|' })
     }
-
 }
-
 module.exports = {
     likesGet,
     likesSet
